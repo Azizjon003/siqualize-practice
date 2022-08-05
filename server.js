d3260d390e927278961ef3e61266b60b8f0701ef
azizjon <aliqulovazizjon68@gmail.com>
2022-08-05 23:43:55 +0500

    saqlandi

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
 const express = require("express");
 const app = express();
-const { Sequelize, logger } = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 
 const sequelize = new Sequelize("learn", "postgres", "13112003", {
   host: "localhost",
   dialect: "postgres",
 });
 
-// const sequelize1 = new Sequelize("sqlite::memory:", {
-//   // Choose one of the logging options
-//   logging: console.log, // Default, displays the first parameter of the log function call
-//   logging: (...msg) => console.log(msg), // Displays all log function call parameters
-//   logging: false, // Disables logging
-//   logging: (msg) => logger.debug(msg), // Use custom logger (e.g. Winston or Bunyan), displays the first parameter
-//   //   logging: logger.debug.bind(logger), // Alternative way to use custom logger, displays all messages
-// });
+const odam = sequelize.define("odam", {
+  name: DataTypes.TEXT,
+  favoriteColor: {
+    type: DataTypes.TEXT,
+    defaultValue: "blue",
+  },
+});
+
+(async () => {
+  await sequelize.sync();
+  // Code here
+})();
+odam
+  .create({
+    name: "Valisher",
+    favoriteColor: "qizil",
+  })
+  .then(() => {
+    console.log("created");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 async function ishla() {
   try {
     await sequelize.authenticate();
